8d6e67387c028490829e6eb07f1a706d19de974d
Tobias Zagorni <tobias.zagorni@student.hpi.de>
2018-02-06 14:10:52 +0100

    morphic: initial mouse support

--- morphic/cLib/myGraphics.c
+++ morphic/cLib/myGraphics.c
@@ -10,38 +10,27 @@ SDL_Texture * texture;
 int winW;
 int winH;
 
+#define MOUSE_DOWN 1
+#define MOUSE_UP 0
+
 struct {
     uint32_t x, y;
+    int left, middle, right;
 } MouseState;
 
-typedef {
-    uint32_t eventType;
+#define EVENT_TYPE_NONE     0
+#define EVENT_TYPE_MOUSE    1
+
+typedef struct {
+    uint32_t type;
     uint32_t timeStamp;
     uint32_t x, y;
     uint32_t buttonState, modifierKeys;
     uint32_t unused[2];
-}
+} MouseEvent;
 
-
-[1]	- event type 1
-[2]	- time stamp
-[3]	- mouse x position
-[4]	- mouse y position
-[5]	- button state; bitfield with the following entries:
-		1	-	yellow (e.g., right) button
-		2	-	blue (e.g., middle) button
-		4	-	red (e.g., left) button
-		[all other bits are currently undefined]
-[6]	- modifier keys; bitfield with the following entries:
-		1	-	shift key
-		2	-	ctrl key
-		4	-	(Mac specific) option key
-		8	-	Cmd/Alt key
-		[all other bits are currently undefined]
-[7]	- reserved.
-[8]	- reserved.
-
-int openWindow(int x, int y, int w, int h){
+int openWindow(int x, int y, int w, int h)
+{
     if(window != NULL)
         return 1;
     
@@ -62,6 +51,8 @@ int openWindow(int x, int y, int w, int h){
     renderer = SDL_CreateRenderer(window, -1, 0);
     texture = SDL_CreateTexture(renderer,
         SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, w, h);
+
+    MouseState.left = MouseState.middle = MouseState.right = MOUSE_UP;
     
     return 0;
 }
@@ -79,27 +70,66 @@ int drawBitmap(char* data, int l, int r, int t, int b)
     return 0;
 }
 
-void closeWindow(){
+void closeWindow()
+{
     SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
-void sendMouseState(uint32_t *e, uint32_t timestamp) {
-    
+void sendMouseState(MouseEvent *SqEv, uint32_t timestamp)
+{
+    SqEv->type = EVENT_TYPE_MOUSE;
+    SqEv->timeStamp = timestamp;
+    SqEv->x = MouseState.x;
+    SqEv->y = MouseState.y;
+    SqEv->buttonState = MouseState.right
+                      | (MouseState.middle << 1)
+                      | (MouseState.left << 2);
+    SqEv->modifierKeys = 0;
 }
 
-void getEvents(uint32_t *e) {
+void handleButtonEvent(uint8_t button, int value)
+{
+    switch (button)
+    {
+    case SDL_BUTTON_LEFT:
+        MouseState.left = value;
+        break;
+    case SDL_BUTTON_MIDDLE:
+        MouseState.middle = value;
+        break;
+    case SDL_BUTTON_RIGHT:
+        MouseState.right = value;
+        break;
+    }
+}
+
+void getEvents(void *e)
+{
     SDL_Event event;
     
-    if (!SDL_PollEvent(&event)) {
-        e[0] = 0;
-        return;
-    }
-    switch (event.type) {
+    if (!SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
         case SDL_MOUSEMOTION:
             MouseState.x = event.motion.x;
             MouseState.y = event.motion.y;
-            break;
+            sendMouseState(e, event.motion.timestamp);
+            return;
+        case SDL_MOUSEBUTTONUP:
+        case SDL_MOUSEBUTTONDOWN:
+            MouseState.x = event.button.x;
+            MouseState.y = event.button.y;
+            handleButtonEvent(
+                    event.button.button,
+                    SDL_MOUSEBUTTONUP ? MOUSE_UP: MOUSE_DOWN);
+            sendMouseState(e, event.button.timestamp);
+            return;
+        }
+    }
+
+    bzero(e, 8 * sizeof(uint32_t));
 }
