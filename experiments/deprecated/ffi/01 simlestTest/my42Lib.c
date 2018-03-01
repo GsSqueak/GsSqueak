59e434312840cb57779e39a66f636015b25994ae
Martin Stamm <martin.stamm@student.hpi.de>
2018-03-01 10:34:02 +0100

    Move old Graffics experiments, all Code for current demo at /morphic

---
+++ experiments/deprecated/ffi/01 simlestTest/my42Lib.c
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+int32_t give42()
+{
+  return 42;
+} 
---
+++ experiments/deprecated/ffi/02 bitmapTest/test.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void printBtitmap();
+
+int main(int argc, char* argv[]) {  	
+
+	printBtitmap();
+} 
