7b1abc60b7c2de067645c6b9699c348cb8eddf4b
Pit Wegner <pit.wegner@student.hpi.de>
2018-04-09 15:53:26 +0200

    add CollectionLiterals

--- SqueakImport/transform.sh
+++ SqueakImport/transform.sh
@@ -1,5 +1,5 @@
-cd SqBlockClosure.package
-find -name '*.st' | xargs sed -i -E 's/<prim.*>/self halt./g'
+cd SqArray.package
+find -name '*.st' | xargs sed -i -E 's/<prim.*>//g'
 for i in *.class;
 do      
         new=$(echo "$i" | sed 's/.class/.extension/')
@@ -7,6 +7,6 @@ do
 done
 for i in $(find -name '*.st');
 do
-	echo "*SqBlockClosure-$(cat $i)" > $i
+	echo "*SqArray-$(cat $i)" > $i
 done
 find -name '*.json' | xargs sed -i -E 's/\\/\\\\/g'
