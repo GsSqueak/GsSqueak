35c127cd52bac1e8de425369762091cce7d60dcf
Pit Wegner <pit.wegner@student.hpi.de>
2018-04-18 12:33:15 +0200

    update some small things

--- SqueakImport/SqKernel.package/BlockClosure.extension/transform.py
+++
@@ -1,8 +0,0 @@
-import glob
-
-for filename in glob.iglob('./instance/*.st'):
-  f=open(filename, 'r')
-  data=f.read()
-  f=open(filename, 'w')
-  print('*SqKernel-' + data.split('-', 1)[-1], file=f)
-
