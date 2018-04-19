bf5d68edea4a5989cfe485176ae7ce9b3ea56178
Stephan Lutz <stephan.lutz@student.hpi.de>
2018-04-19 12:46:40 +0200

    Work on String.

--- SqueakImport/transform.sh
+++ SqueakImport/transform.sh
@@ -1,12 +1,12 @@
-cd SqArray.package
+cd SqCollectionLiterals.package
 find -name '*.st' | xargs sed -i -E 's/<prim.*>//g'
-for i in *.class;
+for i in ByteString.class;
 do      
         new=$(echo "$i" | sed 's/.class/.extension/')
         mv "$i" "$new"
 done
 for i in $(find -name '*.st');
 do
-	echo "*SqArray-$(cat $i)" > $i
+	echo "*SqCollectionLiterals-$(cat $i)" > $i
 done
 find -name '*.json' | xargs sed -i -E 's/\\/\\\\/g'
