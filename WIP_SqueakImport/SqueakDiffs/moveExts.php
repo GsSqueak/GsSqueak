63667c7773f8dbb9b5f0a72c5294719709f87af0
jakob <jakob.braun@posteo.de>
2018-05-30 15:46:09 +0200

    readded deleted files

--- WIP_SqueakImport/SqueakDiffs/moveExts.php
+++ WIP_SqueakImport/SqueakDiffs/moveExts.php
@@ -3,50 +3,27 @@
 $extensionFolders;
 exec("find -name *.extension",$extensionFolders); 
 
-$unmoved = array();
 
 foreach($extensionFolders as $extension){
     $className = basename($extension,".extension");
     $classes = array();
     exec("find -name " . $className . ".mod", $classes);
     if(sizeof($classes) != 1){
-        
-        
-        $newClasses = array();
-        exec("find -name " . $className . ".new", $newClasses);
-        exec("find -name " . $className . ".link", $newClasses);
-        if(sizeof($newClasses) > 0){
-            $unmoved[] =  $extension;
-        }
-        else{ //createClass
-            exec("mkdir ExtensionClasses/" . $className . ".mod");
-            exec("echo \"{name: '$className'}\" > ExtensionClasses/$className.mod/properties.json");
-        }
-            
-        
+        echo $extension . " not moved\n";
     }
     else{
         echo "move" . $classes[0] . "\n";
         
         $instMethods = array();
-        exec("find " . $extension . "/instance/ -name *.st",$instMethods); 
-        
-        exec("mkdir " . $classes[0] . "/instance/");
-        foreach($instMethods as $instMethod){
-            exec("mv -f " . $instMethod . " " . $classes[0] . "/instance/");
-        }
-        
         $classMethods = array();
+        exec("find " . $extension . "/instance/ -name *.st",$instMethods); 
         exec("find " . $extension . "/class/ -name *.st",$classMethods); 
-        exec("mkdir " . $classes[0] . "/class/");
-        foreach($classMethods as $classMethod){
-            exec("mv -f " . $classMethod . " " . $classes[0] . "/class/");
-        }
-        
         
         
     }
+    return;
 }
 
-var_dump($unmoved);
+
+
 ?>
