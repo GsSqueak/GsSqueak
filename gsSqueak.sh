0b5612cfca2aca998951b6b5924fc83ee787959a
pitwegner <pit.wegner@student.hpi.de>
2018-06-13 13:36:07 +0200

    Update gsSqueak.sh

--- gsSqueak.sh
+++ gsSqueak.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 
+stone_name="gsSqueak"
+key="$1"
+case $key in
+    -s|--stone-name)
+      STONE_NAME="$2"
+      shift
+      shift
+      ;;
+    *)    # unknown option
+      install_usage
+      # not supported
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      ;;
+esac
+done
+set -- "${POSITIONAL[@]}"
+
 pushd GsDevKit_home >/dev/null
 source bin/defHOME_PATH.env
 popd >/dev/null
-startTopaz $1 -l << EOF
+startTopaz $stone_name -l << EOF
 login
 run
 Workspace evaluate: '
